0f1b0d4483e2d769a8fc41b040aeeb8ff15c2ad3
Kupreenko Oleg <89996212885@mail.ru>
2022-07-14 15:05:06 +0300

    Completed Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,21 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Договоримся, что числа только целые и будут приниматься рандомно в интервале (-10; 11)
+int NumberA = new Random() .Next(-10, 11);
+Console.WriteLine(NumberA);
+int NumberB = new Random() .Next(-10, 11);
+Console.WriteLine(NumberB);
+int max = 0;
+int min = 0;
+
+if (NumberA > NumberB) 
+{
+    max = NumberA;
+    min = NumberB;
+}
+else 
+{
+    max = NumberB;
+    min = NumberA;
+}
+Console.WriteLine("max =" + max);
+Console.WriteLine("min =" + min);
