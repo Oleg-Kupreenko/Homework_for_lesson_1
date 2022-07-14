72090bfb803d8ec244e94506d9bc3610aad1b3e5
Kupreenko Oleg <89996212885@mail.ru>
2022-07-14 14:58:14 +0300

    add Task_6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Напишите программу, которая на входе принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 // Договоримся, что число натуральное и будет приниматься рандомно в интервале (1; 25)
+int NumberA = new Random() .Next(1, 25);
+Console.WriteLine(NumberA);
+double NumberB = (NumberA / 2);
+Console.WriteLine(NumberB);
+if (NumberA == NumberB * 2)
+{
+    Console.WriteLine ("Да");
+}
+
+else
+    {
+        Console.WriteLine ("нет");
+    }
+
