3507e0d0f16395a9ee75076128c2fc19674c84c5
Kupreenko Oleg <89996212885@mail.ru>
2022-07-14 15:06:40 +0300

    Completed Task_4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,34 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 // Договоримся, что числа только целые и будут приниматься рандомно в интервале (-15; 11)
+int NumberA = new Random() .Next(-15, 11);
+Console.Write($"{NumberA, 3} ");
+int NumberB = new Random() .Next(-15, 11);
+Console.Write($"{NumberB, 3} ");
+int NumberC = new Random() .Next(-15, 11);
+Console.WriteLine($"{NumberC, 3} ");
+int max = 0;
+
+if (NumberA > NumberB)
+{
+    if (NumberA > NumberC)
+    {
+        max = NumberA;
+    }
+    else 
+    {
+        max = NumberC;
+    }
+}
+else 
+{
+    if (NumberB > NumberC)
+    {
+        max = NumberB;
+    }
+    else
+    {
+        max = NumberC;
+    }
+
+}
+Console.WriteLine(max);
